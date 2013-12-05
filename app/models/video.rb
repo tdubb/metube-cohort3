1030260baf6c77b1407237a0a31b8bfb4b00016e
MakerSquare Student <students@themakersquare.com>
2013-12-05 16:27:46 -0600

    finished first extension with average(column_name, *options) method

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,5 +3,23 @@ class Video < ActiveRecord::Base
   has_many :ratings
 
   validates :user_id, :title, :youtube_id, presence: true
-  validates :youtube_id, uniqueness: true  
+  validates :youtube_id, uniqueness: true
+
+  def avg_rating
+ #  	total = 0.0
+ #  	self.ratings.to_a.each do |rating|
+ #  		total += rating.value	
+ #  	end
+  		
+	# if self.ratings.count == 0
+	# 	return 0
+	# else
+ #  		return total / self.ratings.count		
+	# end
+
+	self.ratings.count == 0 ? 0 : self.ratings.average('value')  	
+  end
 end
+
+
+
